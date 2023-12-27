b73bbcd032942ef3bb855e24d56c305e7e79ddc2
maxor <maxor@LAPTOP-67PPUVS9>
2023-12-27 01:14:13 +0000

    Add project files.

---
+++ SchlorrOClock/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace SchlorrOClock
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBOX = new System.Windows.Forms.PictureBox();
+            this.timTIME = new System.Windows.Forms.Timer(this.components);
+            this.mnuTime = new System.Windows.Forms.MenuStrip();
+            this.TimeZoneItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gMTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bSTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eDTToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pDTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cESTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cSTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.picBOX)).BeginInit();
+            this.mnuTime.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picBOX
+            // 
+            this.picBOX.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.picBOX.Location = new System.Drawing.Point(12, 0);
+            this.picBOX.Name = "picBOX";
+            this.picBOX.Size = new System.Drawing.Size(450, 450);
+            this.picBOX.TabIndex = 0;
+            this.picBOX.TabStop = false;
+            this.picBOX.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timTIME
+            // 
+            this.timTIME.Enabled = true;
+            this.timTIME.Interval = 1000;
+            this.timTIME.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // mnuTime
+            // 
+            this.mnuTime.AutoSize = false;
+            this.mnuTime.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.mnuTime.Dock = System.Windows.Forms.DockStyle.None;
+            this.mnuTime.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TimeZoneItem});
+            this.mnuTime.Location = new System.Drawing.Point(0, 0);
+            this.mnuTime.Name = "mnuTime";
+            this.mnuTime.Size = new System.Drawing.Size(91, 25);
+            this.mnuTime.TabIndex = 1;
+            this.mnuTime.Text = "menuStrip1";
+            // 
+            // TimeZoneItem
+            // 
+            this.TimeZoneItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gMTToolStripMenuItem,
+            this.bSTToolStripMenuItem,
+            this.eDTToolStripMenuItem1,
+            this.pDTToolStripMenuItem,
+            this.cESTToolStripMenuItem,
+            this.cSTToolStripMenuItem});
+            this.TimeZoneItem.Name = "TimeZoneItem";
+            this.TimeZoneItem.Size = new System.Drawing.Size(75, 21);
+            this.TimeZoneItem.Text = "Time Zone";
+            // 
+            // gMTToolStripMenuItem
+            // 
+            this.gMTToolStripMenuItem.Name = "gMTToolStripMenuItem";
+            this.gMTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gMTToolStripMenuItem.Text = "GMT";
+            this.gMTToolStripMenuItem.Click += new System.EventHandler(this.gMTToolStripMenuItem_Click_1);
+            // 
+            // bSTToolStripMenuItem
+            // 
+            this.bSTToolStripMenuItem.Checked = true;
+            this.bSTToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bSTToolStripMenuItem.Name = "bSTToolStripMenuItem";
+            this.bSTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bSTToolStripMenuItem.Text = "BST";
+            this.bSTToolStripMenuItem.Click += new System.EventHandler(this.bSTToolStripMenuItem_Click_1);
+            // 
+            // eDTToolStripMenuItem1
+            // 
+            this.eDTToolStripMenuItem1.Name = "eDTToolStripMenuItem1";
+            this.eDTToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.eDTToolStripMenuItem1.Text = "EDT";
+            this.eDTToolStripMenuItem1.Click += new System.EventHandler(this.eDTToolStripMenuItem1_Click);
+            // 
+            // pDTToolStripMenuItem
+            // 
+            this.pDTToolStripMenuItem.Name = "pDTToolStripMenuItem";
+            this.pDTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pDTToolStripMenuItem.Text = "PDT";
+            this.pDTToolStripMenuItem.Click += new System.EventHandler(this.pDTToolStripMenuItem_Click_1);
+            // 
+            // cESTToolStripMenuItem
+            // 
+            this.cESTToolStripMenuItem.Name = "cESTToolStripMenuItem";
+            this.cESTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cESTToolStripMenuItem.Text = "CEST";
+            this.cESTToolStripMenuItem.Click += new System.EventHandler(this.cESTToolStripMenuItem_Click_1);
+            // 
+            // cSTToolStripMenuItem
+            // 
+            this.cSTToolStripMenuItem.Name = "cSTToolStripMenuItem";
+            this.cSTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cSTToolStripMenuItem.Text = "CST";
+            this.cSTToolStripMenuItem.Click += new System.EventHandler(this.cSTToolStripMenuItem_Click_1);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.Controls.Add(this.mnuTime);
+            this.Controls.Add(this.picBOX);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mnuTime;
+            this.Name = "form1";
+            this.Text = "SchlorrOClock";
+            ((System.ComponentModel.ISupportInitialize)(this.picBOX)).EndInit();
+            this.mnuTime.ResumeLayout(false);
+            this.mnuTime.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBOX;
+        private System.Windows.Forms.Timer timTIME;
+        private System.Windows.Forms.MenuStrip mnuTime;
+        private System.Windows.Forms.ToolStripMenuItem TimeZoneItem;
+        private System.Windows.Forms.ToolStripMenuItem gMTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bSTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eDTToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem pDTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cESTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cSTToolStripMenuItem;
+    }
+}
+
---
+++ SchlorrOClock/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchlorrOClock
+{
+    public partial class form1 : Form
+    {
+        int Zonedif = 0;
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.FillEllipse(Brushes.CadetBlue, 0, 0, 449, 449);
+            g.FillEllipse(Brushes.White, 5, 5, 439, 439);
+
+            DateTime now = DateTime.Now;
+            double m = (now.Minute * 6) * Math.PI / 180;
+            double s = (now.Second * 6) * Math.PI / 180;
+            double h = ((((now.Hour + Zonedif) % 12) * 30) + ((m / (Math.PI / 180)) / 12)) * Math.PI / 180;
+            
+            double da = now.Day;
+            string day = Convert.ToString(da).PadLeft(2, '0');
+            double mo = now.Month;
+            string month = Convert.ToString(mo).PadLeft(2, '0');
+            double ye = now.Year;
+            string year = Convert.ToString(ye).Substring(2);
+
+            g.DrawRectangle(Pens.DeepSkyBlue, 180, 250, 25, 15);
+            g.DrawString(day, new Font("Serif", 11.0f), Brushes.CornflowerBlue, 183, 249);
+            g.DrawRectangle(Pens.DeepSkyBlue, 210, 250, 25, 15);
+            g.DrawString(month, new Font("Serif", 11.0f), Brushes.CornflowerBlue, 213, 249);
+            g.DrawRectangle(Pens.DeepSkyBlue, 240, 250, 25, 15);
+            g.DrawString(year, new Font("Serif", 11.0f), Brushes.CornflowerBlue, 243, 249);
+
+            g.DrawLine(Pens.Red, 225, 225, (int)(225 + 140 * Math.Sin(s)), (int)(225 - 140 * Math.Cos(s)));
+            g.DrawLine(Pens.Blue, 225, 225, (int)(225 + 140 * Math.Sin(m)), (int)(225 - 140 * Math.Cos(m)));
+            g.DrawLine(Pens.Blue, 225, 225, (int)(225 + 100 * Math.Sin(h)), (int)(225 - 100 * Math.Cos(h)));
+
+            g.FillEllipse(Brushes.Blue, 220, 220, 10, 10);
+
+            int hour = 0;
+            for(int i = 0; i < 61; i++)
+            {
+                double iangle = ((i * 6) * (Math.PI / 180));
+                if(i%5 == 0)
+                {
+                    g.DrawLine(Pens.PaleVioletRed, (float)(225 + Math.Sin(iangle) * 100), (float)(225 - Math.Cos(iangle) * 100), (float)((225 + Math.Sin(iangle) * 100) + Math.Sin(iangle) * 15), (float)((225 - Math.Cos(iangle) * 100) - Math.Cos(iangle) * 15));
+                    g.DrawLine(Pens.Red, (float)(225 + Math.Sin(iangle) * 200), (float)(225 - Math.Cos(iangle) * 200), (float)((225 + Math.Sin(iangle) * 200) + Math.Sin(iangle) * 20), (float)((225 - Math.Cos(iangle) * 200) - Math.Cos(iangle) * 20));
+                    if(hour > 0)
+                    {
+                        g.DrawString(Convert.ToString(hour), new Font("Serif", 11.0f), Brushes.IndianRed, (float)(225 + Math.Sin(iangle) * 190 - 5), (float)(225 - Math.Cos(iangle) * 180 - 8));
+                    }
+                    hour++;
+                }
+                g.DrawLine(Pens.PaleVioletRed, (float)(225 + Math.Sin(iangle) * 100), (float)(225 - Math.Cos(iangle) * 100), (float)((225 + Math.Sin(iangle) * 100) + Math.Sin(iangle) * 10), (float)((225 - Math.Cos(iangle) * 100) - Math.Cos(iangle) * 10));
+                g.DrawLine(Pens.Red, (float)(225 + Math.Sin(iangle) * 200), (float)(225 - Math.Cos(iangle) * 200), (float)((225 + Math.Sin(iangle) * 200) + Math.Sin(iangle) * 10), (float)((225 - Math.Cos(iangle) * 200) - Math.Cos(iangle) * 10));
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            picBOX.Refresh();
+        }
+
+        private void eDTToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Zonedif = -5;
+            if (!DateTime.Now.IsDaylightSavingTime())
+            {
+                Zonedif++;
+            }
+            bSTToolStripMenuItem.Checked = false;
+            gMTToolStripMenuItem.Checked = false;
+            cSTToolStripMenuItem.Checked = false;
+            cESTToolStripMenuItem.Checked = false;
+            pDTToolStripMenuItem.Checked = false;
+            eDTToolStripMenuItem1.Checked = true;
+        }
+
+        private void gMTToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Zonedif = -1;
+            if (!DateTime.Now.IsDaylightSavingTime())
+            {
+                Zonedif++;
+            }
+            bSTToolStripMenuItem.Checked = false;
+            gMTToolStripMenuItem.Checked = true;
+            cSTToolStripMenuItem.Checked = false;
+            cESTToolStripMenuItem.Checked = false;
+            pDTToolStripMenuItem.Checked = false;
+            eDTToolStripMenuItem1.Checked = false;
+        }
+
+        private void bSTToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Zonedif = 0;
+            if (!DateTime.Now.IsDaylightSavingTime())
+            {
+                Zonedif++;
+            }
+            bSTToolStripMenuItem.Checked = true;
+            gMTToolStripMenuItem.Checked = false;
+            cSTToolStripMenuItem.Checked = false;
+            cESTToolStripMenuItem.Checked = false;
+            pDTToolStripMenuItem.Checked = false;
+            eDTToolStripMenuItem1.Checked = false;
+        }
+
+        private void pDTToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Zonedif = -8;
+            if (!DateTime.Now.IsDaylightSavingTime())
+            {
+                Zonedif++;
+            }
+            bSTToolStripMenuItem.Checked = false;
+            gMTToolStripMenuItem.Checked = false;
+            cSTToolStripMenuItem.Checked = false;
+            cESTToolStripMenuItem.Checked = false;
+            pDTToolStripMenuItem.Checked = true;
+            eDTToolStripMenuItem1.Checked = false;
+        }
+
+        private void cESTToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Zonedif = 1;
+            if (!DateTime.Now.IsDaylightSavingTime())
+            {
+                Zonedif++;
+            }
+            bSTToolStripMenuItem.Checked = false;
+            gMTToolStripMenuItem.Checked = false;
+            cSTToolStripMenuItem.Checked = false;
+            cESTToolStripMenuItem.Checked = true;
+            pDTToolStripMenuItem.Checked = false;
+            eDTToolStripMenuItem1.Checked = false;
+        }
+
+        private void cSTToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Zonedif = 7;
+            if (!DateTime.Now.IsDaylightSavingTime())
+            {
+                Zonedif++;
+            }
+            bSTToolStripMenuItem.Checked = false;
+            gMTToolStripMenuItem.Checked = false;
+            cSTToolStripMenuItem.Checked = true;
+            cESTToolStripMenuItem.Checked = false;
+            pDTToolStripMenuItem.Checked = false;
+            eDTToolStripMenuItem1.Checked = false;
+        }
+
+/*      This was going to be an implementation of Martian Coordinated Time and it does calculate MSD accurately but is slightly too innacurate to get the exact time.
+        private void martianStandardTimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double dt = (DateTime.Now.Year - 1970) * 31556952000;
+            for (int i = 0; i < DateTime.Now.Month; i++)
+            {
+                dt += DateTime.DaysInMonth(DateTime.Now.Year, i) * 86400000;
+            }
+            dt += (DateTime.Now.Day-1) * 86400000;
+            dt += DateTime.Now.Hour * 3600000;
+            dt += DateTime.Now.Minute * 600000;
+            dt += DateTime.Now.Second * 100000;
+
+            dt = 2440587.5 + (dt / 86400000);
+            dt += (69.184) / 86400;
+
+            dt = dt - 2451545;
+
+            dt = ((dt - 4.5) / 1.027491252) + 44796 - 0.00096;
+        }*/
+    }
+}
